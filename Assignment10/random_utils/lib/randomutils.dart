5e1fe1de728815b73fb43bb0a60edcab87044fce
Imran Hussain <imran.hussain@venturedive.com>
2020-07-08 15:44:26 +0500

    Add 2 util methods.

--- Assignment10/random_utils/lib/randomutils.dart
+++ Assignment10/random_utils/lib/randomutils.dart
@@ -1,7 +1,25 @@
 library randomutils;
 
+import 'dart:math';
+
+Random _random = new Random();
+
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class Utils {
+  static int randomColor() {
+    var hex = "0xFF";
+    for (int i = 0; i < 3; i++) {
+      hex += _random.nextInt(255).toRadixString(16).padLeft(2, '0');
+    }
+    return int.parse(hex);
+  }
+
+  static String randomString({int length}) {
+    var codeUnits =
+        new List.generate(length ?? _random.nextInt(pow(2, 10)), (index) {
+      return _random.nextInt(33) + 89;
+    });
+
+    return new String.fromCharCodes(codeUnits);
+  }
 }
