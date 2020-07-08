3a24b278830337de6f7b38d1086f34866f539001
Imran Hussain <imran.hussain@venturedive.com>
2020-07-08 18:11:01 +0500

    Add documentation.

--- Assignment10/random_utils/lib/randomutils.dart
+++ Assignment10/random_utils/lib/randomutils.dart
@@ -2,9 +2,11 @@ library randomutils;
 
 import 'dart:math';
 
-Random _random = new Random();
+Random _random = Random();
 
+/// Provides useful utility functions.
 class Utils {
+  /// Generates a random integer that represents a Hex color.
   static int randomColor() {
     var hex = "0xFF";
     for (int i = 0; i < 3; i++) {
@@ -13,28 +15,33 @@ class Utils {
     return int.parse(hex);
   }
 
+  /// Generates a random string of provided or random length.
   static String randomString({int length}) {
     var codeUnits =
-        new List.generate(length ?? _random.nextInt(pow(2, 10)), (index) {
+        List.generate(length ?? _random.nextInt(pow(2, 10)), (index) {
       return _random.nextInt(33) + 89;
     });
 
-    return new String.fromCharCodes(codeUnits);
+    return String.fromCharCodes(codeUnits);
   }
 
+  /// Cleans up provided string by removing extra whitespace.
   static String condenseWhiteSpace(String str) {
-    return str.replaceAll(new RegExp(r"\s+"), " ").trim();
+    return str.replaceAll(RegExp(r"\s+"), " ").trim();
   }
 
+  /// Removes all whitespace from provided string.
   static String removeWhiteSpace(String str) {
-    return str.replaceAll(new RegExp(r"\s+"), "");
+    return str.replaceAll(RegExp(r"\s+"), "");
   }
 
+  /// Returns true for for all strings that are empty, null or only whitespace.
   static bool isWhiteSpaceOrEmptyOrNull(String str) {
     return removeWhiteSpace(str ?? "").isEmpty;
   }
 
+  /// Extracts decimal numbers from the provided string.
   static String removeNonDigits(String str) {
-    return str.replaceAll(new RegExp(r"\D"), "");
+    return str.replaceAll(RegExp(r"\D"), "");
   }
 }
